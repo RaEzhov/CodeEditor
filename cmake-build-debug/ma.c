75979c14b692ab42facb316f8f3d3f8d40eb46a0
RaEzhov <ezhov.mail@yandex.ru>
2021-05-05 12:54:23 +0300

    Commit

---
+++ cmake-build-debug/ma.c
@@ -0,0 +1,67 @@
+#include<math.h>
+# include    <stdio.h>
+#include    "func.h"
+#define     ABC      100
+
+
+
+#include "string.h"
+
+
+#include<stdlib.h>
+#define       length 100
+
+#                include
+"string.h"
+#define SIN( x)    sin(2* x)
+
+int s = -10;
+int c = 2;
+int f(){
+
+}
+if (a > b){
+for (int f =   0; f < 100; f++){
+a++;
+while(s<0){
+s = s + 3;
+for (int i = 0; i<=12; i++) {
+c++;
+}
+}int a = 0;
+}
+int a =          9;
+//hello world
+
+return 0;
+}
+
+
+
+
+
+
+int main
+        (int argv, char*argc[]) {
+
+    char outName[20];
+    editNameOutFile           (1, outName, argc);
+    printf("%s", outName);
+
+    while (argv > 2
+            ) {
+        printf           ("a"   );
+    }
+    switch (
+
+
+            expression    ) {
+        case 1:
+            open a;
+
+        default:
+            int b = 120;
+    }return     0;
+
+
+}
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define length 100
+
+enum status{ directive = 0, function, init, cycleOrCond, switch_, doWhile };
+
+void editNameOutFile(int argn, char* name, char* argc[]){
+    strcpy(name, "out");
+    strcpy(name+3, argc[argn]);
+}
+
+int main(int argv, char* argc[]) {
+    char outName[20] = {0};
+    editNameOutFile(1, outName, argc);
+    FILE *fl = fopen(argc[1], "rt");
+    FILE *fw = fopen(outName, "wt");
+    char *buffer = (char *) malloc(sizeof(char) * length);
+    int status = 0;
+    int level = 0;
+    int space = 0;
+    int quotes = 0;
+    int endl = 0;
+    char c = 0;
+    char prevC = 0;
+    int swtc = 0;
+    while (!feof(fl)) {
+        fscanf(fl, "%c", &c);
+        if (level == 0 && c == 'i' && prevC == 10) {
+            prevC = '\n';
+            fprintf(fw, "\n");
+        }
+        if (c == '/' && prevC == '/') {
+            fprintf(fw, "/");
+            while (c != 10 && !feof(fl)) {
+                fscanf(fl, "%c", &c);
+                fprintf(fw,"%c", c);
+            }
+        }
+        if (c == '}' && prevC == '}') {
+            continue;
+        }
+        if (c == ':') {
+            swtc++;
+            level++;
+        }
+        if (swtc > 0 && c == ';') {
+            level--;
+            swtc--;
+        }
+        if (c == '{') {
+            level++;
+        }
+        if (prevC == 'e' && (c == '<' || c == '"')) {
+            prevC = ' ';
+            fprintf(fw," ");
+        }
+        if (c == '}') {
+            level--;
+            if (prevC == ';' || prevC == 10 || prevC == '}' || prevC == ' ') {
+                putchar('\b');
+                putchar('\b');
+                putchar('\b');
+                putchar('\b');
+            }
+        }
+        if (c != ' ' && c != 10 && c != 9) {
+            fprintf(fw, "%c", c);
+            if (c == '}') {
+                fprintf(fw,"\n");
+                for (int i = 0; i < level; i++) {
+                    prevC = ' ';
+                    fprintf(fw,"    ");
+                }
+            }
+        }
+        if (c == ' ' && (prevC != ' ' && prevC != 10 && prevC != '#')) {
+            prevC = ' ';
+            fprintf(fw," ");
+        }
+        if (c == 10 && prevC != 10 && prevC != '(' && prevC != ')' && (prevC < 'a' || prevC > 'z') &&
+            (prevC < '1' || prevC > '9')) {
+            fprintf(fw,"\n");
+            for (int i = 0; i < level; i++) {
+                prevC = ' ';
+                fprintf(fw,"    ");
+            }
+        }
+        prevC = c;
+    }
+
+    int k = 0;
+    for(int i = 0; i < 5; i++)
+        while(k>2)
+            k++;
+
+    free(buffer);
+    fclose(fl);
+    fclose(fw);
+
+
+    //open output and count vlojennost cyclov
+
+    fw = fopen(outName, "rt");
+    char buf[length] = {0};
+    int cycle = 0;
+    while (!feof(fw)){
+        fscanf(fw, "%s", buf);
+        if (!(strcmp(buf, "for")) || !(strcmp(buf, "while"))){
+
+        }
+        printf("%s\n", buf);
+    }
+    return 0;
+}
